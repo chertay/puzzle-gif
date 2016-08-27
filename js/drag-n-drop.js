d58fa4b4d4e24435ccddcbcf75cfeea658685896
archie <france.baril@architextus.com>
2016-08-27 16:11:22 -0700

    Drag-n-drop... getting ready to work with new order...

--- js/drag-n-drop.js
+++ js/drag-n-drop.js
@@ -32,11 +32,36 @@ function ddInit(){
    //$('.col').off().on('click', function(){
    //   alert('clicked');
    */
+   
+   function didYouWin(domOrder){
+      alert('dropped');
+      var sortedResults = [];
+      //$('.col').each(function() {
+      //   results.push($(this).attr('id'));
+      //})
+      sortedResults = domOrder.slice();
+      sortedResults.getSorted('.col', 'data-order');
+      alert(domOrder.toString() + ' =? ' + sortedResults.toString());
+      if (domOrder.toString() === sortedResults.toString()) {
+         alert('You win');
+      } else {
+         alert('You lose, you shitty player');
+      }
+   }
+   
+   function getSorted(selector, attrName) {
+    return $($(selector).toArray().sort(function(a, b){
+        var aVal = parseInt(a.getAttribute(attrName)),
+            bVal = parseInt(b.getAttribute(attrName));
+        return aVal - bVal;
+    }));
+}
+   
    $('.flex-grid').sortable({
      stop: function(event, ui) {
-         alert('You dropped a shitty frame!');
          var order = $('.flex-grid').sortable('toArray');
          console.log('new order:', order);
+         didYouWin(order);
      }
    });
 
